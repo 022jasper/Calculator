0ccb60909df59d29ed966056dee5498f0d63741a
Jasper <dnjun80081@gmail.com>
2023-11-11 22:40:37 +0800

    [MOD] add calculator display answer's number

--- lib/display_numbers_view/display_numbers_view.dart
+++ lib/display_numbers_view/display_numbers_view.dart
@@ -10,6 +10,31 @@ class DisplayNumbersView extends StatefulWidget {
 class _DisplayNumbersViewState extends State<DisplayNumbersView> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Column(
+      children: [
+        Expanded(
+          flex: 1,
+          child: Container(
+            width: MediaQuery.of(context).size.width,
+            color: Colors.black,
+            child: const Align(
+              alignment: Alignment.bottomRight,
+              child: Text(
+                '0',
+                style: TextStyle(color: Colors.white, fontSize: 50),
+              ),
+            ),
+          ),
+        ),
+        Expanded(
+          flex: 2,
+          child: Container(
+            width: MediaQuery.of(context).size.width,
+            color: Colors.yellow,
+            child: const Text('C'),
+          ),
+        )
+      ],
+    );
   }
 }
