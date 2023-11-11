abd336200d2147863faf734e3b7e5ad60c612ed3
Jasper <>
2023-11-11 20:01:08 +0800

    [MOD] moving project

---
+++ lib/display_numbers_view/display_numbers_view.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class DisplayNumbersView extends StatefulWidget {
+  const DisplayNumbersView({Key? key}) : super(key: key);
+
+  @override
+  _DisplayNumbersViewState createState() => _DisplayNumbersViewState();
+}
+
+class _DisplayNumbersViewState extends State<DisplayNumbersView> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
